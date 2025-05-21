62a1118a1ba5b32b10833075c0a5fc07abbef295
Gabriel Ramos <gabrieljhramos@gmail.com>
2025-05-21 19:44:42 -0300

    Archivo getJason.py modificado para cumplir con la documentacion.

--- TP06-ingenieria_reversa/getJason.py
+++ TP06-ingenieria_reversa/getJason.py
@@ -1,12 +1,25 @@
-# Decompiled with PyLingual (https://pylingual.io)
-# Internal filename: getJason.py
-# Bytecode version: 3.12.0rc2 (3531)
-# Source timestamp: 2025-05-06 19:05:36 UTC (1746558336)
+"""
+Recuperacion del codigo fuente de getJason.pyc
+
+Este script lee un archivo JSON y muestra en pantalla el valor asociado a una clave específica (por defecto 'token1').
+Se puede especificar la clave como segundo argumento al ejecutar el script.
+Uso:
+    python getJason.py archivo.json [clave]
+"""
 
 import json
 import sys
 jsonfile = sys.argv[1]
-jsonkey = 'token1'
+
+# Modificacion para que tome dos argumentos y se pueda cambiar el token
+if len(sys.argv) == 3:
+    jsonkey = sys.argv[2]
+else:
+    jsonkey = "token1"
+
+# Codigo antiguo
+# jsonkey = "token1"
+
 with open(jsonfile, 'r') as myfile:
     data = myfile.read()
 obj = json.loads(data)
