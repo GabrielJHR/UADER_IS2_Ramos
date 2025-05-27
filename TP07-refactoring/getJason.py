9e409ec72b17c00eccd914dd74d8ec796f9489b1
Gabriel Ramos <gabrieljhramos@gmail.com>
2025-05-26 21:25:32 -0300

    Se creo una clase que tiene el mismo funcionamiento que el codigo original

--- TP07-refactoring/getJason.py
+++ TP07-refactoring/getJason.py
@@ -1,3 +1,42 @@
+import json
+import sys
+
+# Control de branch para evitar conflictos con el código antiguo
+NUEVO_CODIGO = True
+
+class LectorJSON:
+    """
+    Clase para leer un archivo JSON y obtener el valor de una clave específica.
+    """
+
+    def __init__(self, jsonfile, jsonkey='token1'):
+        self.jsonfile = jsonfile
+        self.jsonkey = jsonkey
+
+    def get_token(self):
+        """
+        Lee el archivo JSON y devuelve el valor asociado a la clave especificada.
+        """
+        with open(self.jsonfile, 'r') as myfile:
+            data = myfile.read()
+        obj = json.loads(data)
+        return str(obj[self.jsonkey])
+    
+if NUEVO_CODIGO:
+    # Si se está ejecutando el nuevo código, se crea una instancia de LectorJSON
+    if len(sys.argv) == 3:
+        lector = LectorJSON(sys.argv[1], sys.argv[2])
+    else:
+        lector = LectorJSON(sys.argv[1])
+    
+    print("Nuevo código en ejecución")
+    print(lector.get_token())
+    sys.exit(0)
+    
+
+
+"""-------------------------------------------------- Programa original ----------------------------------------------------"""
+
 """
 Recuperacion del codigo fuente de getJason.pyc
 
@@ -6,9 +45,9 @@ Se puede especificar la clave como segundo argumento al ejecutar el script.
 Uso:
     python getJason.py archivo.json [clave]
 """
-
-import json
-import sys
+# Las librerias se importan al principio del archivo
+# import json
+# import sys
 jsonfile = sys.argv[1]
 
 # Modificacion para que tome dos argumentos y se pueda cambiar el token
